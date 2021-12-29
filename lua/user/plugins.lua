214d54090b833454e55bf16c46b811e7f2ef8688
alfredosdpiii <alfredo.sdpiii@gmail.com>
2021-12-29 16:51:58 +0800

    Added project manager

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -77,6 +77,19 @@ return packer.startup(function(use)
 
   --no distractions mode
   use "junegunn/goyo.vim"
+    use "othree/javascript-libraries-syntax.vim"
+    use {
+  "ahmedkhalf/project.nvim",
+  config = function()
+    require("project_nvim").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      require('telescope').load_extension('projects')
+    }
+  end
+}
+
 
 
 
