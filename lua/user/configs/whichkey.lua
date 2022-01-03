7ab0b183a0f10ab251727566267739909f0e3be2
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-01-03 17:59:40 +0800

    whichkey on top

--- lua/user/configs/whichkey.lua
+++ lua/user/configs/whichkey.lua
@@ -44,7 +44,7 @@ local setup = {
   },
   window = {
     border = "rounded", -- none, single, double, shadow
-    position = "bottom", -- bottom, top
+    position = "top", -- bottom, top
     margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
     padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
     winblend = 0,
