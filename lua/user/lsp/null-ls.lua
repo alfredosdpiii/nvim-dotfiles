274b162287c5cc537bed7e1aa8241b7a81d8ffb9
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-01-05 16:13:22 +0800

    added linter

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -10,10 +10,16 @@ local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
 	debug = false,
+    on_attach = function(client)
+        if client.resolved_capabilities.document_formatting then
+            vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+        end
+    end,
 	sources = {
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
+        formatting.stylint,
     -- diagnostics.flake8
 	},
 })
